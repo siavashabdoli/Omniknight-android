5bde3570682702d35755f3d1d6f0890ec19e7722
Arash Khangaldi <khangaldi.arash@gmail.com>
2016-12-03 22:51:05 +0330

    Setting onclick listener with RxBinding and Retrolambda

--- app/src/main/java/app/arsh/omniknightapp/MainActivity.java
+++ app/src/main/java/app/arsh/omniknightapp/MainActivity.java
@@ -2,11 +2,45 @@ package app.arsh.omniknightapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.jakewharton.rxbinding.view.RxView;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView textView = (TextView) findViewById(R.id.textView);
+    RxView.clicks(textView).subscribe(t -> showMessage());
+  }
+
+  public void showMessage() {
+    Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+    Observable
+        .just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        .filter(integer -> integer % 2 == 0)
+        .map(this::multiple)
+        .takeUntil(integer -> integer.length() > 0)
+        .subscribeOn(Schedulers.computation())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(System.out::println);
+
+    Observable
+        .just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        .subscribe(System.out::println);
+  }
+
+  public String multiple(int input) {
+    return "Number is :"+input;
+  }
+
+  public int print() {
+
+    return 0;
   }
 }
