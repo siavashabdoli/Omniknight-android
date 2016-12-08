86c824de2de0233e283b5ec21dfba233d1c15dae
Arash Khangaldi <khangaldi.arash@gmail.com>
2016-12-09 00:44:18 +0330

    Updating gradle version

---
+++ app/src/main/java/app/arsh/omniknightapp/CustomItemAnimator.java
@@ -0,0 +1,10 @@
+package app.arsh.omniknightapp;
+
+import android.support.v7.widget.SimpleItemAnimator;
+
+/**
+ * Created by arash on 12/8/16.
+ */
+
+public class CustomItemAnimator extends SimpleItemAnimator {
+}
