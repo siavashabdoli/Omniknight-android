f70901d22f9bd2dcca4e6f5b25c4a1a1ed53c814
Arash Khangaldi <khangaldi.arash@gmail.com>
2017-01-26 10:55:32 +0330

    Adding MenuItem to Toolbar

--- app/src/main/java/app/arsh/omniknightapp/view/activities/MainActivity.java
+++ app/src/main/java/app/arsh/omniknightapp/view/activities/MainActivity.java
@@ -5,10 +5,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import app.arsh.omniknightapp.R;
 import app.arsh.omniknightapp.model.LocationManager;
@@ -42,8 +41,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityInter
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    setSupportActionBar(toolbar);
     ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
     presenter = new MainActivityPresenter(this, this);
 
     RxView.clicks(fab)
@@ -51,6 +50,22 @@ public class MainActivity extends AppCompatActivity implements MainActivityInter
             fabClicked());
 
     presenter.onCreateViewFinished();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the main_menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == R.id.extra) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 
   @Override public void loadCities(List<Country> countryList) {
